49ccbe918f8bdbe66a12c6b72ac99bc0e76de9b9
sanjit samar <sanjit1525@gmail.com>
2023-10-02 02:43:23 +0530

    code refactored
    code refactored

--- src/index.js
+++ src/index.js
@@ -1,10 +1,109 @@
 import React from "react";
-import ReactDOM  from "react-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-function App(){
-    return <h1>Hello There !</h1>
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Header() {
+  return (
+    <header className="header">
+      <h1>MY ONLINE PIZZA STORE</h1>
+    </header>
+  );
+}
 
-root.render(<App/>);
+function Menu() {
+  const pizzas = pizzaData;
+  const numberOfPizza = pizzas.length;
+
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      {pizzas && numberOfPizza > 1 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => {
+            return <Pizza pizzaObj={pizza} key={pizza.name} />;
+          })}
+        </ul>
+      )}
+    </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <h3>{props.pizzaObj.name}</h3>
+      <p>{props.pizzaObj.ingredients}</p>
+      <p>{props.pizzaObj.price}</p>
+    </li>
+  );
+}
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="order">
+        <h1>We are Open, Order Now ! </h1>
+        <h2>{new Date().toLocaleTimeString()}</h2>
+        <button className="btn">Order Now</button>
+      </div>
+    </footer>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
